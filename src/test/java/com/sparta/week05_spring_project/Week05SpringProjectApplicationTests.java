e7caf099466e87bf95eaf351942d5d9d3ec2a95b
hyeonorof <hyeonorof@gmail.com>
2022-07-17 10:02:15 +0900

    Initial commit

---
+++ src/test/java/com/sparta/week05_spring_project/Week05SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week05_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week05SpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
